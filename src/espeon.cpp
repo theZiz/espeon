226ac74aea04b681e8d8c7d46c0af20fc0b6089c
Alexander Matthes <ziz@mailbox.org>
2020-05-15 10:34:35 +0200

    Extended stub

--- src/espeon.cpp
+++ src/espeon.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <signal.h>
+#include <chrono>
+#include <thread>
+
+volatile sig_atomic_t stop;
+
+void inthand(int signum)
+{
+	stop = 1;
+	std::cout << std::endl;
+}
 
 int main()
 {
-	std::cout << "Espeon!" << std::endl;
+	std::cout << "Running Espeon..." << std::endl;
+
+	/* main loop which check for tcp packages and forwards them to the connected
+	 * back ends. */
+	std::cout << "Stop server with Ctrl+C..." << std::endl;
+	signal(SIGINT, inthand);
+	while ( !stop )
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	}
+	std::cout << "Espeon finished." << std::endl;
 	return 0;
 }
